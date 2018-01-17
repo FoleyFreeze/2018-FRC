4ece97a129cef57aeff92583a34b7652bfe006fe
21slucido <21slucido>
2018-01-16 21:01:18 -0500

    SL added Arm Class

--- src/org/usfirst/frc/team910/robot/components/DriveTrain.java
+++ src/org/usfirst/frc/team910/robot/components/DriveTrain.java
@@ -8,7 +8,7 @@ import org.usfirst.frc.team910.robot.io.Sensors;
 public class DriveTrain {
 	public static final double DYN_BRAKE_KP = 0.1; // This is power per inch of error
 	public static final double DRIVE_STRAIGHT_KP = 0.1; // Distance difference by inch
-	public static final double DRIVE_STRAIGHTNAVX_KP = 0.68;  //Distance difference by inch
+	public static final double DRIVE_STRAIGHTNAVX_KP = 0.1;  //Distance difference by inch
 
 	Outputs out;
 	
---
+++ src/org/usfirst/frc/team910/robot/io/Arm.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team910.robot.io;
+
+public class Arm {
+	
+	public Arm() {
+
+	}
+
+	public void run() {
+
+	}
+
+}
