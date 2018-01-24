52402e77b508bbc949777e84c6afa471d7fee389
18nbadanjek <18nbadanjek>
2018-01-23 19:30:22 -0500

    NB Created WIP framework to begin Auton 1/23

--- src/org/usfirst/frc/team910/robot/auton/AutonMain.java
+++ src/org/usfirst/frc/team910/robot/auton/AutonMain.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.usfirst.frc.team910.robot.auton.steps.AutonStep;
 import org.usfirst.frc.team910.robot.auton.steps.DriveForward;
+import org.usfirst.frc.team910.robot.auton.steps.EndStep;
 
 public class AutonMain {
 
@@ -14,11 +15,19 @@ public class AutonMain {
 	public AutonMain() {
 		steps = new ArrayList<AutonStep>();
 		steps.add(new DriveForward());
+		steps.add(new EndStep());
 		
 			
 	}
 	public void run() {
 		steps.get(currentStep).run();
+		if(steps.get(currentStep).isError()) {
+			currentStep = steps.size() - 1;
+		}
+		if(steps.get(currentStep).isDone()) {
+			currentStep++;
+		}
+
 	}
 
 }
