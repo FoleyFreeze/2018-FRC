6aaf160d06b1b99bea7aac58d58a7cdf8340ccfb
18nbadanjek <18nbadanjek>
2018-01-29 20:38:04 -0500

    NB debugged MP to not go from >5in/s to 0in/s in one milisecond

--- src/org/usfirst/frc/team910/robot/util/Path.java
+++ src/org/usfirst/frc/team910/robot/util/Path.java
@@ -6,10 +6,15 @@ public class Path {
 
 	public ArrayList<Double> positions; //array of the recorded positions per loop
 	public ArrayList<Double> velocities;//array of the recorded velocities per loop
+	
 
 	public static final double TOP_VELOC = 60.1; // inches per second, highest capable velocity of robot
 	public static final double ACCEL = 60; // inches per second squared
 	public static final double DT = 0.01; // 10 milliseconds
+	
+	//tolerance constants
+	public static final double POS_TOL = 0.01;
+	public static final double VEL_TOL = 0.01;
 
 	public Path() { //constructor to create the arrays
 		positions = new ArrayList<Double>();
@@ -57,8 +62,9 @@ public class Path {
 					double transDist = (dist -step1Dist) - prevPos;
 					//time that we should begin slowing down
 					double transTime = transDist/TOP_VELOC;
-					
-					double transPos = prevPos+TOP_VELOC*transTime;
+					//part from previous position to the decel point
+					//double transPos = prevPos+TOP_VELOC*transTime;
+					double transPos = dist - step1Dist;
 					position = transPos + TOP_VELOC*(DT-transTime) - 0.5 * ACCEL * (DT - transTime) * (DT - transTime);
 					velocity = TOP_VELOC - ACCEL*(DT-transTime);
 				}
@@ -67,14 +73,16 @@ public class Path {
 				velocities.add(velocity); //record velocity
 
 			}
-			velocity = TOP_VELOC;
 			
 			while (position < dist) { //Step 3: Decelerate, while position is less than the total path
 				position = position + velocity * DT - 0.5 * ACCEL * DT * DT; //distance formula with negative acceleration 
 				velocity = velocity - ACCEL * DT;//velocity formula with negative acceleration
+				if(velocity <= 0) {
+					velocity = 0;
+				}
 				
 				//if the time step went past end point, stop at the end point 
-				if (position > dist) {
+				if (position >= dist - POS_TOL && velocity <= 0) {
 					position = dist;
 					velocity = 0;
 				}
@@ -84,17 +92,49 @@ public class Path {
 			}
 			
 		} else { //if robot will not reach top speed before it needs to slow down, use 2 step method
+		
+			double step1Dist = (velocMax*velocMax)/(2*ACCEL); //record the position during first phase,
+			
 			while (velocity < velocMax) { //Step 1: Accelerate, while velocity has not reached highest capable velocity (not robot's top speed)
 				position = position + velocity * DT + 0.5 * ACCEL * DT * DT; //distance formula, x = xi + vit + .5at^2
 				velocity = velocity + ACCEL * DT; //velocity formula, vf = vi+at
+				
+				//if the time step passed the point to slow down, change so that it is slowing down
+				if (position > (dist - step1Dist)) {
+					//position at which you start slowing down
+					double prevPos = positions.get(positions.size()-1);
+					//distance covered before slowing down;
+					double transDist = (dist -step1Dist) - prevPos;
+					//time that we should begin slowing down
+					double transTime = transDist/velocMax;
+					//part from previous position to the decel point
+					//double transPos = prevPos+TOP_VELOC*transTime;
+					double transPos = dist - step1Dist;
+					position = transPos + velocMax*(DT-transTime) - 0.5 * ACCEL * (DT - transTime) * (DT - transTime);
+					velocity = velocMax - ACCEL*(DT-transTime);
+					positions.add(position);//record position
+					velocities.add(velocity);//record velocity
+					break;
+				}
 				
 				positions.add(position);//record position
 				velocities.add(velocity);//record velocity
 			}
 			
+			
 			while (velocity > 0) { //Step 2: Decelerate, while velocity is greater than zero
 				position = position + velocity * DT - 0.5 * ACCEL * DT * DT; //distance formula w/ negative acceleration
 				velocity = velocity - ACCEL * DT; //velocity formula with negative acceleration
+				
+				if(velocity <= 0) {
+					velocity = 0;
+				}
+				
+				//if the time step went past end point, stop at the end point 
+				if (position >= dist - POS_TOL && velocity <= 0) {
+					position = dist;
+					velocity = 0;
+				}
 
 				positions.add(position);//record position
 				velocities.add(velocity);//record position
