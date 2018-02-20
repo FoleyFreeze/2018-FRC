3560a0e7bd228a5d3cef4f94e9889c057761216d
Delta <Delta@team910.com>
2018-02-19 23:33:07 -0500

    NB fixed electroback with actual can IDs

--- src/org/usfirst/frc/team910/robot/io/ElectroBach.java
+++ src/org/usfirst/frc/team910/robot/io/ElectroBach.java
@@ -8,19 +8,19 @@ public class ElectroBach {
 	public static final int RIGHT_DRIVE_CAN_2=14;
 	public static final int RIGHT_DRIVE_CAN_3=15;
 	
-	public static final int ARM_CAN_1=-1;
-	public static final int ARM_CAN_2=-1;
+	public static final int ARM_CAN_1=10;
+	public static final int ARM_CAN_2=11;
 	
-	public static final int ELEVATOR_CAN_1=-1; //TODO fix for 2018
-	public static final int ELEVATOR_CAN_2=-1;
+	public static final int ELEVATOR_CAN_1=6;
+	public static final int ELEVATOR_CAN_2=9;
 
-	public static final int LEFT_GATHER_CAN=-1; //TODO fix for 2018
-	public static final int RIGHT_GATHER_CAN=-1;//TODO fix for 2018
+	public static final int LEFT_GATHER_CAN=4; //TODO fix for 2018
+	public static final int RIGHT_GATHER_CAN=5;//TODO fix for 2018
 	
 	public static final double TICKS_PER_INCH = 317.2;
 	public static final double TICKS_PER_DEGREE = 0; //TODO calculate or measure real value 
 	public static final double TICKS_PER_INCH_HEIGHT = 1; //TODO calculate or measure real value please
 	
-	public static final int CLIMBER_CAN_1=-1;
-	public static final int CLIMBER_CAN_2=-1;
+	public static final int CLIMBER_CAN_1=3;
+	public static final int CLIMBER_CAN_2=12;
 }
