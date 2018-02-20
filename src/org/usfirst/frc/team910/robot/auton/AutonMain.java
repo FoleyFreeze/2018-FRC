7db6225f15fcd68b142cb31607ca74dbdd38fa38
18nbadanjek <18nbadanjek>
2018-02-19 19:50:23 -0500

    NB working on game data for auton

--- src/org/usfirst/frc/team910/robot/auton/AutonMain.java
+++ src/org/usfirst/frc/team910/robot/auton/AutonMain.java
@@ -3,31 +3,50 @@ package org.usfirst.frc.team910.robot.auton;
 import java.util.ArrayList;
 
 import org.usfirst.frc.team910.robot.auton.steps.AutonStep;
-import org.usfirst.frc.team910.robot.auton.steps.DriveForward;
 import org.usfirst.frc.team910.robot.auton.steps.DrivePath;
 import org.usfirst.frc.team910.robot.auton.steps.EndStep;
 import org.usfirst.frc.team910.robot.auton.steps.StartStep;
+
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class AutonMain {
 
 	private ArrayList<AutonStep> steps;
 	
+	public boolean left = false;
+	public boolean right = false;
+	public boolean zig = false;
+	public boolean zag = false;
+	String gameData;
 	
 	public int currentStep = 0;
 	
 	public AutonMain() {
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		
+		if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'L') {
+			left = true;
+		}else if(gameData.charAt(0) == 'R' && gameData.charAt(1) == 'R') {
+			right = true;
+		}else if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'R' ) {
+			zig = true;
+		}else {
+			zag = true;
+		}
+		
+		
 		//declare a new array for our steps
 		steps = new ArrayList<AutonStep>();
 		//new steps
 		steps.add(new StartStep());//always first step
 		steps.add(new DrivePath());
 		steps.add(new EndStep());//always last step
-		
-		
 	}
 	
 	public void init() {
+
 		currentStep = 0;
+		
 	}
 	
 	public void run() {
