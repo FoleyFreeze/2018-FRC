8e6fbeaec9af549e65611f5c4f0152302da97942
18nbadanjek <18nbadanjek>
2018-02-24 17:04:05 -0500

    NB moved fms stuff to inputs

--- src/org/usfirst/frc/team910/robot/auton/AutonMain.java
+++ src/org/usfirst/frc/team910/robot/auton/AutonMain.java
@@ -2,15 +2,14 @@ package org.usfirst.frc.team910.robot.auton;
 
 import java.util.ArrayList;
 
+import org.usfirst.frc.team910.robot.Component;
 import org.usfirst.frc.team910.robot.auton.steps.AutonStep;
 import org.usfirst.frc.team910.robot.auton.steps.DrivePath;
 import org.usfirst.frc.team910.robot.auton.steps.EndStep;
 import org.usfirst.frc.team910.robot.auton.steps.StartStep;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class AutonMain {
+public class AutonMain extends Component {
 
 	private ArrayList<AutonStep> steps;
 	
@@ -19,50 +18,25 @@ public class AutonMain {
 	final String PositionCenter = "Center";
 	final String PositionRight = "Right";
 	
-	public boolean goalsLeft = false;
-	public boolean goalsRight = false;
-	public boolean goalsZig = false;
-	public boolean goalsZag = false;
-
-	String gameData;
 	
 	public int currentStep = 0;
 	
 	public AutonMain() {
-		
 		//declare a new array for our steps
-		steps = new ArrayList<AutonStep>();
+		steps = new ArrayList<AutonStep>();	
 		
-		
-		
-		if(goalsLeft) {
+		if(sense.goalsLeft) {
 			//new steps
 			steps.add(new StartStep());//always first step
 			steps.add(new DrivePath());
 			steps.add(new EndStep());//always last step
-		} 
-		
-		
+		}		
 	}
+	
 	
 	public void init() {
 		
 		currentStep = 0;
-		
-		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		
-		String positionSelected = SmartDashboard.getString("Position Selector", startPosition);
-		System.out.println("Position Selected: " + positionSelected);
-		
-		if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'L') {
-			goalsLeft = true;
-		} else if(gameData.charAt(0) == 'R' && gameData.charAt(1) == 'R') {
-			goalsRight = true;
-		} else if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'R' ) {
-			goalsZig = true;
-		} else {
-			goalsZag = true;
-		}
 		
 	}
 	
