085f19854c7e3789678e010925fce9788bd600f9
19esmith <19esmith>
2018-02-26 21:08:09 -0500

    Add ability to turn on or off saving object info

--- src/org/usfirst/frc/team910/robot/vision/VisionObjectDataPixy2Listener.java
+++ src/org/usfirst/frc/team910/robot/vision/VisionObjectDataPixy2Listener.java
@@ -9,7 +9,20 @@ public class VisionObjectDataPixy2Listener implements TableEntryListener {
 
 	public void valueChanged(NetworkTable table, java.lang.String key, NetworkTableEntry entry, NetworkTableValue value, int flags) {
 	
-		// TODO: Need to parse pixy1objdata data and put in queue
+		if (saveValue2 == true ) {
+			// TODO: Need to parse the pixy2objdata data and put in queue
+			
+		}
 	}
 	
+	// Set the object save mode pixy 2
+	// flag == true, save object data for pixy2; flag == false, ignore the info.
+	public void saveValues(boolean flag) {
+		
+		if(flag) { saveValue2 = true; } else { saveValue2 = false; }
+		
+	}
+	
+	private boolean saveValue2;
+
 }
