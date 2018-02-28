04d1890d73a284736b9fc801874a2db37fc9cad4
18SMoozhayil <18SMoozhayil>
2018-02-27 21:07:43 -0500

    add queue and parse the received object data message

--- src/org/usfirst/frc/team910/robot/vision/VisionObjectDataPixy1Listener.java
+++ src/org/usfirst/frc/team910/robot/vision/VisionObjectDataPixy1Listener.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team910.robot.vision;
 
+import java.util.concurrent.BlockingQueue;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.TableEntryListener;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -8,15 +10,50 @@ import edu.wpi.first.networktables.NetworkTableValue;
 
 public class VisionObjectDataPixy1Listener implements TableEntryListener {
 
+	public VisionObjectDataPixy1Listener(BlockingQueue queue) {
+		
+		this.queue = queue;
+		
+	}
+	
 	public void valueChanged(NetworkTable table, java.lang.String key, NetworkTableEntry entry, NetworkTableValue value,
 			int flags) {
-		if (saveValue1 == true) {
-			// TODO: Need to parse the pixy2objdata data and put in queue
 
+		String msg; // TODO: Data from Network Table goes here from entry "pixy1objdata"
+		msg = "Test,Test"; // TODO: Data from Network Table goes here from entry "pixy1objdata"
+		String[] msg_parsed = msg.split(",");
+		int numBlocks = 0;
+        int BLOCK_SIZE = 6;
+        int frameNum = 0;
+        
+		VisionData newData;
+
+		// Check if there are any blocks in this message
+		numBlocks = Integer.parseInt(msg_parsed[2]);
+
+		if (numBlocks > 0 ) {
+
+			frameNum = Integer.parseInt(msg_parsed[1]);
+			
+			// Convert message data into VisionData object data
+			for (int i = 0; i < numBlocks; i++) {
+				
+				newData = new VisionData();
+				newData.blockID = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 0)) + 2]); // block ID
+				newData.sig = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 1)) + 2]); // signature
+				newData.x = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 2)) + 2]); // x
+				newData.y = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 3)) + 2]); // y
+				newData.w = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 4)) + 2]); // width
+				newData.h = Integer.parseInt(msg_parsed[(i * (BLOCK_SIZE + 5)) + 2]); // height
+				newData.frame = frameNum;
+				
+			}
+			
+			// Save this in the queue
 		}
+		
 	}
 
-	// TODO: Need to parse the pixy1objdata data and put in queue
 	// Set the object save mode pixy1
 	// flag == true, save object data for pixy2; flag == false, ignore the info.
 	public void saveValues(boolean flag) {
@@ -30,4 +67,6 @@ public class VisionObjectDataPixy1Listener implements TableEntryListener {
 	}
 
 	private boolean saveValue1;
+	private BlockingQueue queue;
+	
 }
