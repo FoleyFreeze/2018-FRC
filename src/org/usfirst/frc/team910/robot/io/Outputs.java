e76ad5ac1719598cd71217789a6b9a95fea68c05
Delta <Delta@team910.com>
2018-03-13 21:25:13 -0400

    GS took out system exit if encoders were broken

--- src/org/usfirst/frc/team910/robot/io/Outputs.java
+++ src/org/usfirst/frc/team910/robot/io/Outputs.java
@@ -122,13 +122,13 @@ public class Outputs extends Component {
 	public void resetEncoders() {
 		if(leftDrive1.setSelectedSensorPosition(0, 0, 50) != ErrorCode.OK) {
 			System.out.println("Error in resetting left encoder position");
-			//TODO: instead of crashing, propegate this error to auton isError() 
+			//TODO: instead of crashing, propagate this error to auton isError() 
 			//in order to prevent auton from running when encoders are broken
-			System.exit(-1); 
+			//System.exit(-1); 
 		}
 		if(rightDrive1.setSelectedSensorPosition(0, 0, 50) != ErrorCode.OK) {
 			System.out.println("Error in resetting right encoder position");
-			System.exit(-1);
+			//System.exit(-1);
 		}
 		
 		armMotor1.setSelectedSensorPosition(0, 0, 0);
