295f6e10cfabf8f28ef4f85a852c0cf5c4d7bec5
Delta <Delta@team910.com>
2018-03-30 00:45:31 -0400

    calibrated new gatherer. setup IR RESPIN state.

--- src/org/usfirst/frc/team910/robot/io/Inputs.java
+++ src/org/usfirst/frc/team910/robot/io/Inputs.java
@@ -86,8 +86,8 @@ public class Inputs extends Component {
 	public void read() {
 		double leftYAxis = -leftStick.getY();
 		double rightYAxis = -rightStick.getY();
-		manualGatherLeft = leftStick.getThrottle();
-		manualGatherRight = rightStick.getThrottle();
+		//manualGatherLeft = leftStick.getThrottle();
+		//manualGatherRight = rightStick.getThrottle();
 		
 		enableMP = false;
 		
